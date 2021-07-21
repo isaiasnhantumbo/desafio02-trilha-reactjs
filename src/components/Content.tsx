e2e92bed69082800ea1f83f93e0b43651bb082f4
Isaias Nhantumbo <isaiasnhantumbo54@gmail.com>
2021-07-21 17:02:48 +0200

    :tada: Added MoviesContext on Content

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,19 @@
+import { useContext } from "react";
+import { MoviesContext } from "../MoviesContext";
+import { Header } from "./Header";
+import { MoviesList } from "./MoviesList";
+
+import '../styles/content.scss';
+
 export function Content() {
-  // Complete aqui
+  const {movies, selectedGenre} = useContext(MoviesContext);
+  
+  return (
+    <div className="container">
+     <Header selectedGenreTitle={selectedGenre.title}/>
+      <main>
+       <MoviesList movies={movies}/>
+      </main>
+    </div>
+  );
 }
