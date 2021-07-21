e8344453e87cec2577b0ce35a11a1b3ac0aedd9c
Isaias Nhantumbo <isaiasnhantumbo54@gmail.com>
2021-07-21 17:01:46 +0200

    :tada: Added MoviesContext on Sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,33 @@
+import { useContext } from 'react';
+import { MoviesContext } from '../MoviesContext';
+import { Button } from './Button';
+
+import '../styles/sidebar.scss';
+
 export function SideBar() {
-  // Complete aqui
+  const { selectNewGenre, selectedGenreId, genres } = useContext(MoviesContext);
+
+  function handleClickButton(id: number) {
+    selectNewGenre(id);
+  }
+
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
