3e07bd06e6554b82943e68b1d079faf897312035
Isaias Nhantumbo <isaiasnhantumbo54@gmail.com>
2021-07-21 17:02:20 +0200

    :tada: Added MoviesList

---
+++ src/components/MoviesList.tsx
@@ -0,0 +1,32 @@
+import { MovieCard } from './MovieCard';
+
+interface Movie {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface MoviesListProps {
+  movies: Movie[];
+}
+
+export function MoviesList({ movies }: MoviesListProps) {
+  return (
+    <div className="movies-list">
+      {movies.map((movie) => (
+        <MovieCard
+          key={movie.imdbID}
+          title={movie.Title}
+          poster={movie.Poster}
+          runtime={movie.Runtime}
+          rating={movie.Ratings[0].Value}
+        />
+      ))}
+    </div>
+  );
+}
